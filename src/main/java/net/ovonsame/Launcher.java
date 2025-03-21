4e43109d67c5b2ed489e8cf0613949234506b2d8
OVNSAME <162483465+OVNSAME@users.noreply.github.com>
2025-03-21 18:52:44 +0300

    Added

---
+++ src/main/java/net/ovonsame/Launcher.java
@@ -0,0 +1,29 @@
+package net.ovonsame;
+
+import net.mcreator.blockly.data.BlocklyLoader;
+import net.mcreator.plugin.JavaPlugin;
+import net.mcreator.plugin.Plugin;
+import net.mcreator.plugin.events.PreGeneratorsLoadingEvent;
+import net.ovonsame.config.element.PluginElementType;
+import net.ovonsame.config.ui.blockly.PluginBlocklyEditorType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Launcher extends JavaPlugin {
+   public static final Logger LOGGER = LogManager.getLogger("Modded Elements");
+
+   public Launcher(Plugin plugin) {
+      super(plugin);
+      this.addListener(PreGeneratorsLoadingEvent.class, (e) -> {
+         BlocklyLoader.addBuiltinCategory("config_screen");
+         BlocklyLoader.addBuiltinCategory("config_category");
+         BlocklyLoader.addBuiltinCategory("config_entries");
+         BlocklyLoader.addBuiltinCategory("config_utils");
+         BlocklyLoader.addBuiltinCategory("config_requirements");
+         BlocklyLoader.addBuiltinCategory("config_variables");
+         BlocklyLoader.INSTANCE.addBlockLoader(PluginBlocklyEditorType.CONFIG);
+         PluginElementType.load();
+      });
+      LOGGER.info("Modded Elements plugin was loaded");
+   }
+}
---
+++ src/main/java/net/ovonsame/config/blockly/data/PluginToolboxType.java
@@ -0,0 +1,7 @@
+package net.ovonsame.config.blockly.data;
+
+import net.mcreator.blockly.data.ToolboxType;
+
+public class PluginToolboxType {
+   public static ToolboxType CONFIG = new ToolboxType("config");
+}
